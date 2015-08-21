d019929e1fdbd684de80d09b4f42f5b2a9ae4638
Daniel Llinas <danllinas@gmail.com>
2015-08-21 13:11:08 -0400

    adding more foundation features.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,23 +18,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      flash[:success] = "User was successfully created."
+      redirect_to @user
     else
+      flash[:alert] = "There was a problem creating the user account."
       render :new
     end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      flash[:success] = 'User was successfully updated.'
+      redirect_to @user
     else
+      flash[:alert] = "There was a problem updated the user information."
       render :edit
     end
   end
 
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    flash[:success] = 'User was successfully destroyed.'
+    redirect_to users_url
   end
 
   private
